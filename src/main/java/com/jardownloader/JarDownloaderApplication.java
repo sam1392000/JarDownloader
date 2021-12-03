f9c7a357259bfe40794df207aca27598211eb36b
Sam Raj <18euit130@skcet.ac.in>
2021-12-03 21:52:01 +0530

    Base Template

---
+++ src/main/java/com/jardownloader/JarDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.jardownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jardownloader/JarDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jardownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
